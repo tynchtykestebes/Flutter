ccc0a705d988b4e3015442d6334e6a6b9ef8e990
tynchtykestebes <tynchtyk.estebesuulu.bj@gmail.com>
2023-06-18 21:53:36 +0600

    finished

--- bilimduu_zhashtar_tapshyrma_5/lib/home/home.dart
+++ bilimduu_zhashtar_tapshyrma_5/lib/home/home.dart
@@ -10,13 +10,13 @@ class DiceApp extends StatefulWidget {
 }
 
 class _DiceAppState extends State<DiceApp> {
-  int firstDice = Random().nextInt(6) +1;
-  int secondDice = Random().nextInt(6) +1;
+  int firstDice = Random().nextInt(6) + 1;
+  int secondDice = Random().nextInt(6) + 1;
 
-  void change(){
+  void change() {
     setState(() {
-      firstDice = Random().nextInt(6) +1;
-      secondDice = Random().nextInt(6) +1;
+      firstDice = Random().nextInt(6) + 1;
+      secondDice = Random().nextInt(6) + 1;
     });
   }
 
@@ -28,36 +28,60 @@ class _DiceAppState extends State<DiceApp> {
         elevation: 0,
         centerTitle: true,
         backgroundColor: Colors.white,
-        title: const Text('Тапшырма 5',
-        style: TextStyle(color: Colors.black,
-        fontSize: 22),),),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              if (firstDice == secondDice)
-              Container(color: Colors.white,
+        title: const Text(
+          'Тапшырма 5',
+          style: TextStyle(color: Colors.black, fontSize: 22),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (firstDice == secondDice)
+              Container(
+                color: Colors.white,
                 width: 105,
                 height: 35,
                 child: const Center(
-                  child: Text('Ура! Куш!',
-                  style: TextStyle(fontSize: 22,
-                  fontWeight: FontWeight.bold),),
-                ),),
-                const SizedBox(height: 25,),
-            Row(children: [
-              const SizedBox(width: 25,),
-              Expanded(child: InkWell(onTap: () {                
-                change();
-              }, child: Image.asset('assets/images/dice$firstDice.png'))),
-              const SizedBox(width: 25,),
-              Expanded(child: InkWell(onTap: () {
-                change();
-              }, child: Image.asset('assets/images/dice$secondDice.png'))),
-              const SizedBox(width: 25,),
-            ],)
-          ],),
+                  child: Text(
+                    'Ура! Куш!',
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            const SizedBox(
+              height: 25,
+            ),
+            Row(
+              children: [
+                const SizedBox(
+                  width: 25,
+                ),
+                Expanded(
+                    child: InkWell(
+                        onTap: () {
+                          change();
+                        },
+                        child:
+                            Image.asset('assets/images/dice$firstDice.png'))),
+                const SizedBox(
+                  width: 25,
+                ),
+                Expanded(
+                    child: InkWell(
+                        onTap: () {
+                          change();
+                        },
+                        child:
+                            Image.asset('assets/images/dice$secondDice.png'))),
+                const SizedBox(
+                  width: 25,
+                ),
+              ],
+            )
+          ],
         ),
+      ),
     );
   }
 }
