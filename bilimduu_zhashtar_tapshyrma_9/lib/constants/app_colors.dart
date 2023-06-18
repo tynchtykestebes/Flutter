b438288bb161584d02b26f36b3b19e471d3f736c
tynchtykestebes <tynchtyk.estebesuulu.bj@gmail.com>
2023-06-18 22:07:31 +0600

    :weather app

--- bilimduu_zhashtar_tapshyrma_9/lib/constants/app_colors.dart
+++ bilimduu_zhashtar_tapshyrma_9/lib/constants/app_colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/rendering.dart';
 
 class AppColors{
-  static const appBgc = Color.fromRGBO(244, 243, 243, 1);
+  static const appBgc = Color.fromRGBO(243, 243, 243, 1);
   static const iconColor = Color.fromRGBO(255, 255, 255, 1);
 }
