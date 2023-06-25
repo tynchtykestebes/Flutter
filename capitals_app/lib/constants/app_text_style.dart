81c8f1d2637780ec4aaa50d34a66bb47a3c50646
tynchtykestebes <tynchtyk.estebesuulu.bj@gmail.com>
2023-06-25 21:14:32 +0600

    second page

---
+++ capitals_app/lib/constants/app_text_style.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyle {
+  static const TextStyle incorrecQuantity = TextStyle(
+    color: Colors.red,
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle totalQuantity = TextStyle(
+    color: Colors.black,
+    fontSize: 22,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle correcQuantity = TextStyle(
+    color: Colors.green,
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle attempts = TextStyle(
+    color: Colors.black,
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+  );
+}
--- capitals_app/lib/view/test_view.dart
+++ capitals_app/lib/view/test_view.dart
@@ -1,4 +1,5 @@
 import 'package:capitals_app/constants/app_colors.dart';
+import 'package:capitals_app/constants/app_text_style.dart';
 import 'package:flutter/material.dart';
 
 class TestView extends StatefulWidget {
@@ -17,19 +18,62 @@ class _TestViewState extends State<TestView> {
         backgroundColor: AppColors.appBarBgc,
         actions: [
           Container(
-            height: 50,
-            width: 80,
-            decoration: const BoxDecoration(
+            height: 30,
+            width: 100,
+            decoration: BoxDecoration(
               color: Colors.white,
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  '0',
+                  style: AppTextStyle.incorrecQuantity,
+                ),
+                Text(
+                  '32',
+                  style: AppTextStyle.totalQuantity,
+                ),
+                Text(
+                  '0',
+                  style: AppTextStyle.correcQuantity,
+                ),
+              ],
             ),
           ),
-          const SizedBox(width: 60,),
-          const Text('3'),
-          const SizedBox(width: 60,),
-          const Icon(Icons.favorite),
-          const Icon(Icons.favorite),
-          const Icon(Icons.favorite),
+          const SizedBox(
+            width: 40,
+          ),
+          const Text(
+            '3',
+            style: AppTextStyle.attempts,
+          ),
+          const SizedBox(
+            width: 40,
+          ),
+          const Icon(
+            Icons.favorite,
+            color: Colors.red,
+          ),
+          const Icon(
+            Icons.favorite,
+            color: Colors.red,
+          ),
+          const Icon(
+            Icons.favorite,
+            color: Colors.red,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.more_vert,
+            ),
+          ),
         ],
+      ),
+      body: const Column(
+        children: [],
       ),
     );
   }
